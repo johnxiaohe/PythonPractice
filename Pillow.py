5dcadd9ac6d8c18b1ab07007b74afc568bb08d26
hexiaoyuan <john_xiaohe@163.com>
2019-06-25 21:07:55 +0800

    pillow图片处理简单学习

--- Pillow.py
+++ Pillow.py
@@ -3,8 +3,8 @@
 
 #图像处理标准库
 
-from PIL import Image,ImageFilter,ImageDraw,ImageFont
-import random
+from PIL import Image,ImageFilter,ImageDraw,ImageFont,ImageGrab
+import random,time
 
 #打开图像文件
 im = Image.open("C:/Users/john/Pictures/timg.jpg")
@@ -16,10 +16,10 @@ print("Original image size:{0:.1f},{1:}".format(w,h))
 im.thumbnail((w/2,h/2))
 print("Resize image to :%s,%s"%(w/2,h/2))
 #保存缩放
-im.save("C:/Users/john/Pictures/timg2k.jpg","jpeg")
+im.save("C:/Users/john/Pictures/pillow/timg2k.jpg","jpeg")
 
 im2=im.filter(ImageFilter.BLUR)
-im2.save("C:/Users/john/Pictures/blur.jpg","jpeg")
+im2.save("C:/Users/john/Pictures/pillow/blur.jpg","jpeg")
 
 #随机字母
 def rndChar():
@@ -48,4 +48,34 @@ for t in range(4):
     draw.text((60*t+10,10),rndChar(),font=font,fill=rndColor2())
 #模糊
 # image=image.filter(ImageFilter.BLUR)
-image.save("C:/Users/john/Pictures/code.jpg","jpeg")
+image.save("C:/Users/john/Pictures/pillow/code.jpg","jpeg")
+
+#屏幕抓取
+print("等待两秒")
+time.sleep(2)
+#全屏抓取
+ImageGrab.grab().save("C:/Users/john/Pictures/pillow/截屏.jpg")
+print("抓屏完毕")
+
+#指定范围抓取
+img = ImageGrab.grab(bbox=(0,100,100,200))#前面两个是左上角坐标，后面两个是右下角坐标
+img.save("C:/Users/john/Pictures/pillow/box.jpg")
+
+#打开图片
+im_rgb=Image.open("C:/Users/john/Pictures/pillow/截屏.jpg")
+
+#半透明百分之五十（128/255）
+im_rgba=im_rgb.copy()
+im_rgba.putalpha(128)
+im_rgba.save("C:/Users/john/Pictures/pillow/半透明.png")
+
+#形状透明
+im_a = Image.new("L",im_rgb.size,255)#白色区块
+draw=ImageDraw.Draw(im_a)#实例化im_a的画笔
+draw.rectangle((200,100,300,200),fill=0,outline=0)#fill=黑色（透明） outlin=无边框 画出该矩形
+im_rgbb=im_rgb.copy()
+im_rgbb.putalpha(im_a)
+im_rgbb.save("C:/Users/john/Pictures/pillow/局部透明.png")
+
+
+
