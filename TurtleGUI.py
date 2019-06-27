844789959ae42b2941d3dc10858c182777c6e723
hexiaoyuan <john_xiaohe@163.com>
2019-06-27 21:11:39 +0800

    海龟绘图turtle补充

--- TurtleGUI.py
+++ TurtleGUI.py
@@ -7,7 +7,9 @@ import turtle
 
 # 设置笔刷宽度:
 turtle.width(4)
+turtle.shape("turtle")#设置光标样式  小乌龟  默认小箭头
 
+#circle(100)  画圆方法 100是半径
 #矩形
 # # 前进:
 # turtle.forward(200)
@@ -33,11 +35,14 @@ turtle.width(4)
 #     turtle.goto(x,y)#画笔起始起始坐标
 #     turtle.pd()#画笔落下  pendown
 #     turtle.seth(0)
+#
 #     for i in range(5):
 #         turtle.fd(200)#就是forward
 #         turtle.rt(144)#右转角度
-# # for x in range(0,250,50):
-# drawStar(0,0)
+# for x in range(0,250,50):
+#       turtle.begin_fill()#填充颜色
+#       drawStar(0,0)
+#       turtle.end_fill()#结束填充颜色
 
 #树
 # 设置色彩模式是RGB:
